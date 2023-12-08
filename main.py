1887e2e8974920135559c14ecaa703409af93980
zofass <chachachka@gmail.com>
2023-12-08 10:46:25 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def is_even(number):
+    return str(number)[-1] in '02468'
+
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print("OK")
